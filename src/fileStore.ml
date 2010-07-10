eda13102804d00875305e39150b65b1009c910ca
katzchang <katzchang@gmail.com>
2010-07-10 14:18:24 +0900

    can get what you set

--- src/fileStore.ml
+++ src/fileStore.ml
@@ -1,2 +1,11 @@
-let empty = 0
-let get store key = Some "hoge"
+let empty = []
+
+let get store key = 
+  try
+    Some (List.assoc key store)
+  with Not_found ->
+    None
+
+let set store key value = 
+  (key,value) :: store
+
--- test/fileStore_test.ml
+++ test/fileStore_test.ml
@@ -4,6 +4,11 @@ open FileStore
 let _ = run_test_tt_main begin "fileStore.ml" >::: [
   "存在しないキーでgetするとNoneがかえる" >:: begin fun () ->
     assert_equal None (get empty "hoge")
-  end
+  end;
+  "setしたものがgetできる"  >:: begin fun () ->
+    let store =
+      set empty "keyfuga" "fuga"
+    in
+    assert_equal (Some "fuga") (get store "keyfuga")
+  end;
 ] end
-
